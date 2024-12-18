19909880ead283569bc49d3c278af01b5d0556ec
Ayta Dzhivanov <kurt.alive.1994@gmail.com>
2024-12-18 15:43:58 +0300

    task_3 done

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 from customers.models import Customer
+from robots.models import Robot
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
-    robot_serial = models.CharField(max_length=5,blank=False, null=False)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE, related_name="orders")
+    robot = models.ForeignKey(Robot,on_delete=models.CASCADE, related_name="orders")
--- robots/models.py
+++ robots/models.py
@@ -5,6 +5,7 @@ class Robot(models.Model):
     model = models.CharField(max_length=2, blank=False, null=False)
     version = models.CharField(max_length=2, blank=False, null=False)
     created = models.DateTimeField(blank=False, null=False)
+    stock = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"{self.model} {self.version}"
