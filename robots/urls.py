a1db0cab3da9ed4b34a91b1d4f93dc5798fcd196
Ayta Dzhivanov <kurt.alive.1994@gmail.com>
2024-12-18 09:44:37 +0300

    task_2 done

--- robots/urls.py
+++ robots/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from robots.views import create_robot
+from robots.views import create_robot, download_production_report
 
 urlpatterns = [
     path('api/create-robot/', create_robot, name='create_robot'),
+    path('api/download-production-report/', download_production_report, name='download_production_report')
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,10 +1,13 @@
 import json
+import openpyxl
 
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.core.exceptions import ValidationError
-
+from django.db import models
+from openpyxl.styles import Alignment
+from django.http import HttpResponse
 from robots.models import Robot
 
 @csrf_exempt
@@ -41,3 +44,48 @@ def create_robot(request):
         return JsonResponse({"error": "Validation error.", "details": e.message_dict}, status=400)
     except Exception as e:
         return JsonResponse({"error": "An unexpected error occurred.", "details": str(e)}, status=500)
+
+def generate_production_report():
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.title = "Temporary"
+
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=7)
+
+    robots = (
+        Robot.objects.filter(created__range=(start_date, end_date))
+        .values("model", "version")
+        .annotate(total=models.Count("id"))
+        .order_by("model", "version")
+    )
+
+    grouped_data = {}
+    for robot in robots:
+        model = robot["model"]
+        if model not in grouped_data:
+            grouped_data[model] = []
+        grouped_data[model].append({"version": robot["version"], "total": robot["total"]})
+    
+    for model, versions in grouped_data.items():
+        new_sheet = workbook.create_sheet(title=model)
+        new_sheet.append(["Модель", "Версия", "Количество за неделю"])
+        for col in ["A", "B", "C"]:
+            new_sheet[f"{col}1"].alignment = Alignment(horizontal="center", vertical="center")
+        
+        for version_data in versions:
+            new_sheet.append([model, version_data["version"], version_data["total"]])
+    
+    if 'Temporary' in workbook.sheetnames:
+        del workbook['Temporary']
+    
+    return workbook
+
+def download_production_report(request):
+    workbook = generate_production_report()
+    
+    response = HttpResponse(content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    response["Content-Disposition"] = 'attachment; filename="production_report.xlsx"'
+    
+    workbook.save(response)
+    return response
